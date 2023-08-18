ee909a570b165bfc43bf159ec87dba962e6b9d46
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-08-18 15:16:02 +0200

    location module uses location lib's  internal methods now

--- firstlook/src/modules/location/location.c
+++ firstlook/src/modules/location/location.c
@@ -13,10 +13,11 @@
 
 #include "message_channel.h"
 #include "modem/lte_lc.h"
+#include "nrf_cloud_coap_transport.h"
+#include "net/nrf_cloud_coap.h"
 
 LOG_MODULE_REGISTER(location_module, CONFIG_APP_MODULE_LOCATION_LOG_LEVEL);
 
-static K_SEM_DEFINE(location_lib_sem, 1, 1);
 static K_SEM_DEFINE(modem_init_sem, 0, 1);
 static K_SEM_DEFINE(trigger_sem, 0, 1);
 #define STACKSIZE               4096
@@ -26,17 +27,7 @@ static void location_event_handler(const struct location_event_data *event_data)
 void location_module_entry(void) {
     int err = 0;
     struct location_config config = { 0 };
-    enum location_method chosen_methods[] = {
-#if defined(CONFIG_LOCATION_METHOD_CELLULAR)
-        LOCATION_METHOD_CELLULAR,
-#endif
-#if defined(CONFIG_LOCATION_METHOD_WIFI)
-        LOCATION_METHOD_WIFI,
-#endif
-#if defined(CONFIG_LOCATION_METHOD_GNSS)
-        LOCATION_METHOD_GNSS,
-#endif
-    };
+
     k_sem_take(&modem_init_sem, K_FOREVER);
 
     err = location_init(location_event_handler);
@@ -54,13 +45,11 @@ void location_module_entry(void) {
 
     while (true) {
         k_sem_take(&trigger_sem, K_FOREVER);
-        for (size_t i = 0; i < ARRAY_SIZE(chosen_methods); ++i) {
-            location_config_defaults_set(&config, 1, &chosen_methods[i]);
-            k_sem_take(&location_lib_sem, K_FOREVER);
-            err = location_request(&config);
-            if (err) {
-                LOG_ERR("Unable to send location request: %d", err);
-            }
+        location_config_defaults_set(&config, 0, NULL);
+        config.mode = LOCATION_REQ_MODE_ALL;
+        err = location_request(&config);
+        if (err) {
+            LOG_ERR("Unable to send location request: %d", err);
         }
     }
 }
@@ -94,6 +83,7 @@ static void location_event_handler(const struct location_event_data *event_data)
             event_data->location.latitude,
             event_data->location.longitude,
             event_data->location.accuracy);
+        // TODO send gnss location
 		break;
 	case LOCATION_EVT_RESULT_UNKNOWN:
 		LOG_DBG("Getting location completed with undefined result");
@@ -104,26 +94,6 @@ static void location_event_handler(const struct location_event_data *event_data)
 	case LOCATION_EVT_ERROR:
 		LOG_WRN("Getting location failed");
 		break;
-	case LOCATION_EVT_GNSS_ASSISTANCE_REQUEST:
-		LOG_DBG("Requested A-GPS data");
-		break;
-	case LOCATION_EVT_GNSS_PREDICTION_REQUEST:
-		LOG_DBG("Requested P-GPS data");
-		break;
-	case LOCATION_EVT_CLOUD_LOCATION_EXT_REQUEST:
-		LOG_DBG("Getting cloud location request");
-#if defined(CONFIG_LOCATION_METHOD_CELLULAR)
-        if (event_data->cloud_location_request.cell_data) {
-            LOG_DBG("%d cells", event_data->cloud_location_request.cell_data->ncells_count);
-        }
-#endif
-#if defined(CONFIG_LOCATION_METHOD_WIFI)
-        if (event_data->cloud_location_request.wifi_data) {
-            LOG_DBG("%d aps", event_data->cloud_location_request.wifi_data->cnt);
-        }
-#endif
-        location_cloud_location_ext_result_set(LOCATION_EXT_RESULT_UNKNOWN, NULL);
-		break;
 	default:
 		LOG_DBG("Getting location: Unknown event %d", event_data->id);
 		break;
@@ -132,7 +102,5 @@ static void location_event_handler(const struct location_event_data *event_data)
         event_data->id == LOCATION_EVT_RESULT_UNKNOWN ||
         event_data->id == LOCATION_EVT_TIMEOUT ||
         event_data->id == LOCATION_EVT_ERROR) {
-        /* request completed */
-        k_sem_give(&location_lib_sem);
     }
 }
