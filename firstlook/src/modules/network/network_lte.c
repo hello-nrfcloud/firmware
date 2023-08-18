f62db09a5c4f6efb56a5e8b0488f6b32bdd5aa13
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-08-18 15:16:27 +0200

    network module waits until datetime is available

--- firstlook/src/modules/network/network_lte.c
+++ firstlook/src/modules/network/network_lte.c
@@ -8,6 +8,7 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/zbus/zbus.h>
 #include <zephyr/net/conn_mgr_connectivity.h>
+#include "date_time.h"
 
 #include "message_channel.h"
 
@@ -21,6 +22,7 @@ LOG_MODULE_REGISTER(network, CONFIG_MQTT_SAMPLE_NETWORK_LOG_LEVEL);
 
 static struct net_mgmt_event_callback l4_cb;
 static struct net_mgmt_event_callback conn_cb;
+static bool network_connected_pending;
 
 static void l4_event_handler(struct net_mgmt_event_callback *cb,
 			     uint32_t event,
@@ -28,12 +30,18 @@ static void l4_event_handler(struct net_mgmt_event_callback *cb,
 {
 	int err;
 	enum network_status status;
+	int64_t timestamp;
 
 	switch (event) {
 	case NET_EVENT_L4_CONNECTED:
 		LOG_INF("IP Up");
 		status = NETWORK_CONNECTED;
-		break;
+		err = date_time_now(&timestamp);
+		if (err) {
+			LOG_INF("NETWORK_CONNECTED is sent later when time is aquired.");
+			network_connected_pending = true;
+			return;
+		}
 	case NET_EVENT_L4_DISCONNECTED:
 		LOG_INF("IP down");
 		status = NETWORK_DISCONNECTED;
@@ -61,6 +69,22 @@ static void connectivity_event_handler(struct net_mgmt_event_callback *cb,
 	}
 }
 
+static void date_time_handler(const struct date_time_evt *evt) {
+	int err;
+	enum network_status status = NETWORK_CONNECTED;
+
+	if (evt->type != DATE_TIME_NOT_OBTAINED && network_connected_pending) {
+		network_connected_pending = false;
+		LOG_INF("time aquired, sending NETWORK_CONNECTED");
+		err = zbus_chan_pub(&NETWORK_CHAN, &status, K_SECONDS(1));
+		if (err) {
+			LOG_ERR("zbus_chan_pub, error: %d", err);
+			SEND_FATAL_ERROR();
+		}
+	}
+}
+
+
 static void network_task(void)
 {
 	int err;
@@ -73,6 +97,8 @@ static void network_task(void)
 
 	net_mgmt_init_event_callback(&conn_cb, connectivity_event_handler, CONNECTIVITY_EVENT_MASK);
 	net_mgmt_add_event_callback(&conn_cb);
+
+	date_time_register_handler(date_time_handler);
 
 	LOG_INF("Bringing network interface up");
 
