f0dc928acc772218c1795afdd712612e31838d86
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2024-06-24 17:01:41 +0200

    tests: on_target: utils: flash_tools: add dfu feature
    This patch adds features to the flash_tools that are needed for the
    dfu test: Preparing a JLink, performing serial dfu with the customized
    dfu script and specifying serial numbers to handle multiple devices.
    
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- tests/on_target/utils/flash_tools.py
+++ tests/on_target/utils/flash_tools.py
@@ -13,10 +13,10 @@ logger = get_logger()
 
 SEGGER = os.getenv('SEGGER')
 
-def reset_device():
-    logger.info(f"Resetting device, segger: {SEGGER}")
+def reset_device(serial=SEGGER):
+    logger.info(f"Resetting device, segger: {serial}")
     try:
-        result = subprocess.run(['nrfutil', 'device', 'reset', '--serial-number', SEGGER], check=True, text=True, capture_output=True)
+        result = subprocess.run(['nrfutil', 'device', 'reset', '--serial-number', serial], check=True, text=True, capture_output=True)
         logger.info("Command output:")
         logger.info(result.stdout)
         logger.info("Command completed successfully.")
@@ -27,13 +27,11 @@ def reset_device():
         logger.info(e.stderr)
         raise
 
-def flash_device(hexfile):
+def flash_device(hexfile, serial=SEGGER, extra_args=[]):
     # hexfile (str): Full path to file (hex or zip) to be programmed
-    logger.info(f"Flashing device, segger: {SEGGER}, firmware: {hexfile}")
+    logger.info(f"Flashing device, segger: {serial}, firmware: {hexfile}")
     try:
-        result = subprocess.run(['nrfutil', 'device', 'program', '--firmware', hexfile, '--serial-number', SEGGER], check=True, text=True, capture_output=True)
-        logger.info("Command output:")
-        logger.info(result.stdout)
+        result = subprocess.run(['nrfutil', 'device', 'program', *extra_args, '--firmware', hexfile, '--serial-number', serial], check=True, text=True, capture_output=True)
         logger.info("Command completed successfully.")
     except subprocess.CalledProcessError as e:
         # Handle errors in the command execution
@@ -42,4 +40,45 @@ def flash_device(hexfile):
         logger.info(e.stderr)
         raise
 
-    reset_device()
+    reset_device(serial)
+
+def recover_device(serial=SEGGER, core="Application"):
+    logger.info(f"Recovering device, segger: {serial}")
+    try:
+        result = subprocess.run(['nrfutil', 'device', 'recover', '--serial-number', serial, '--core', core], check=True, text=True, capture_output=True)
+        logger.info("Command completed successfully.")
+    except subprocess.CalledProcessError as e:
+        # Handle errors in the command execution
+        logger.info("An error occurred while recovering the device.")
+        logger.info("Error output:")
+        logger.info(e.stderr)
+        raise
+
+def dfu_device(zipfile, serial):
+    logger.info(f"Performing MCUBoot DFU, firmware: {zipfile}")
+    thingy91x_dfu = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'thingy91x_dfu.py')
+    # call thingy91x-dfu
+    try:
+        result = subprocess.run(['python3', thingy91x_dfu, '--serial', serial, '--debug', '--image', zipfile], check=True, text=True, capture_output=True)
+        logger.info("Command output:")
+        logger.info(result.stdout)
+        logger.info(result.stderr)
+        logger.info("Command completed successfully.")
+    except subprocess.CalledProcessError as e:
+        # Handle errors in the command execution
+        logger.info("An error occurred while flashing the device.")
+        logger.info("Error output:")
+        logger.info(e.stderr)
+        raise
+
+def setup_jlink(serial):
+    # run command and check if it was successful
+    try:
+        result = subprocess.run(['/opt/setup-jlink/setup-jlink.bash', serial], check=True, text=True, capture_output=False)
+        logger.info("Command completed successfully.")
+    except subprocess.CalledProcessError as e:
+        # Handle errors in the command execution
+        logger.info("An error occurred while setting up JLink.")
+        logger.info("Error output:")
+        logger.info(e.stderr)
+        raise
