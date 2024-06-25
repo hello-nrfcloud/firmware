f3c1930ea00b72d8d89e6cda651536e0d788de52
Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>
2024-06-25 13:32:02 +0200

    tests: on_target: button press test
    Add button press test.
    
    Signed-off-by: Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>

--- tests/on_target/tests/test_uart_output.py
+++ tests/on_target/tests/test_uart_output.py
@@ -6,6 +6,7 @@
 import pytest
 import time
 import os
+import re
 from utils.flash_tools import flash_device, reset_device
 import sys
 sys.path.append(os.getcwd())
@@ -15,12 +16,50 @@ logger = get_logger()
 
 TEST_TIMEOUT = 1 * 60
 
+
+def check_uart_data(uart, expected_patterns):
+    start = time.time()
+    while True:
+        all_patterns_found = True
+        missing_pattern = None
+        for pattern in expected_patterns:
+            if not pattern.search(uart.log):
+                all_patterns_found = False
+                missing_pattern = pattern.pattern
+                break
+
+        if all_patterns_found or (time.time() > start + TEST_TIMEOUT):
+            break
+
+        time.sleep(5)
+
+    assert all_patterns_found, f"Timeout Expired, missing log line pattern: {missing_pattern}"
+
 @pytest.mark.dut1
 def test_program_board_and_check_uart(t91x_board, hex_file):
+    t91x_board.uart.flush()
     flash_device(os.path.abspath(hex_file))
 
-    expected_lines = ["Network connectivity established", "Connected to Cloud"]
-
-    logger.info("Waiting for expected lines on UART")
+    expected_lines = ["Network connectivity established",
+                        "Connected to Cloud",
+                        "trigger: trigger_poll_work_fn: Sending trigger poll message"
+                    ]
     t91x_board.uart.wait_for_str(expected_lines, timeout=TEST_TIMEOUT)
-    logger.info("Expected lines found")
+
+    patterns_boot = [
+            re.compile(r"battery: sample: State of charge: ([\d.]+)"),
+            re.compile(r"environmental_module: sample: temp: ([\d.]+); press: ([\d.]+); humidity: ([\d.]+); iaq: ([\d.]+); CO2: ([\d.]+); VOC: ([\d.]+)"),
+            re.compile(r"transport: state_connected_ready_run: Sending payload to cloud: ([0-9a-fA-Fx]+), len: (\d+)")
+        ]
+    check_uart_data(t91x_board.uart, patterns_boot)
+
+
+    t91x_board.uart.flush()
+    # Simulate button press
+    t91x_board.uart.write("zbus button_press\r\n")
+
+    patterns_button_press = patterns_boot.copy()
+    patterns_button_press.insert(0, re.compile(r"trigger: frequent_poll_run: Button 1 pressed in frequent poll state, restarting duration timer"))
+    patterns_button_press.append(re.compile(r"location_module: location_event_handler: Got location: lat: ([\d.]+), lon: ([\d.]+), acc: ([\d.]+)"))
+
+    check_uart_data(t91x_board.uart, patterns_button_press)
