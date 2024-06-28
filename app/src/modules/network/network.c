012572bac46721029129daeb8e1670f3d40c5f81
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2024-06-28 19:21:02 +0200

    modules: network: Add Energy Estimate reporting
    The network module now reports energy estimate periodically to the
    Connection Information LWM2M object.
    
    To achieve this, the network module is now made to listen to the trigger
    channel and upon receiving a trigger, it retrieves the energy estimate
    using the link control API and encodes it into cbor data and sends it to
    the payload channel. Tested and found to work with hello nrfcloud.
    
    Unit test added.
    
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

--- app/src/modules/network/network.c
+++ app/src/modules/network/network.c
@@ -9,13 +9,24 @@
 #include <zephyr/zbus/zbus.h>
 #include <zephyr/net/conn_mgr_connectivity.h>
 #include <zephyr/net/conn_mgr_monitor.h>
+#include <zephyr/task_wdt/task_wdt.h>
+#include <date_time.h>
+#include <zephyr/smf.h>
 
+#include "modem/lte_lc.h"
+#include "modules_common.h"
+#include "conn_info_object_encode.h"
 #include "message_channel.h"
 
 /* Register log module */
 LOG_MODULE_REGISTER(network, CONFIG_APP_NETWORK_LOG_LEVEL);
 
-/* This module does not subscribe to any channels */
+/* Register subscriber */
+ZBUS_SUBSCRIBER_DEFINE(network, CONFIG_APP_NETWORK_MESSAGE_QUEUE_SIZE);
+
+/* Observe trigger channel */
+ZBUS_CHAN_ADD_OBS(TRIGGER_CHAN, network, 0);
+ZBUS_CHAN_ADD_OBS(TIME_CHAN, network, 0);
 
 /* Macros used to subscribe to specific Zephyr NET management events. */
 #define L4_EVENT_MASK (NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED)
@@ -24,6 +35,46 @@ LOG_MODULE_REGISTER(network, CONFIG_APP_NETWORK_LOG_LEVEL);
 /* Zephyr NET management event callback structures. */
 static struct net_mgmt_event_callback l4_cb;
 static struct net_mgmt_event_callback conn_cb;
+
+
+/* State machine */
+
+/* Module states.
+ *
+ * STATE_INIT: The module is initializing and waiting for time to be available.
+ * STATE_SAMPLING: The  module is ready to sample upon receiving a trigger.
+ */
+enum network_module_state {
+	STATE_INIT,
+	STATE_SAMPLING,
+};
+
+/* State object.
+ * Used to transfer data between state changes.
+ */
+struct s_object {
+	/* This must be first */
+	struct smf_ctx ctx;
+
+	/* Last channel type that a message was received on */
+	const struct zbus_channel *chan;
+};
+/* Forward declarations of state handlers */
+static void state_init_run(void *o);
+static void state_sampling_run(void *o);
+
+static struct s_object s_obj;
+static const struct smf_state states[] = {
+	[STATE_INIT] =
+		SMF_CREATE_STATE(NULL, state_init_run, NULL,
+				 NULL,	/* No parent state */
+				 NULL), /* No initial transition */
+	[STATE_SAMPLING] =
+		SMF_CREATE_STATE(NULL, state_sampling_run, NULL,
+				 NULL,
+				 NULL),
+};
+
 
 static void l4_event_handler(struct net_mgmt_event_callback *cb,
 			     uint32_t event,
@@ -64,9 +115,110 @@ static void connectivity_event_handler(struct net_mgmt_event_callback *cb,
 	}
 }
 
+
+
+static void sample_energy_estimate(void)
+{
+	int64_t system_time;
+	struct payload payload = { 0 };
+	struct energy_estimate energy_estimate = { 0 };
+	int ret;
+
+	struct lte_lc_conn_eval_params conn_eval_params;
+
+	ret = lte_lc_conn_eval_params_get(&conn_eval_params);
+	__ASSERT_NO_MSG(ret == 0);
+
+	LOG_DBG("Energy estimate: %d", conn_eval_params.energy_estimate);
+
+	ret = date_time_now(&system_time);
+	if (ret) {
+		LOG_ERR("Failed to convert uptime to unix time, error: %d", ret);
+		return;
+	}
+
+	energy_estimate.bt = (int32_t)(system_time / 1000);
+	energy_estimate.vi = conn_eval_params.energy_estimate;
+
+	ret = cbor_encode_conn_info_object(payload.string, sizeof(payload.string),
+					&energy_estimate, &payload.string_len);
+	if (ret) {
+		LOG_ERR("Failed to encode conn info object, error: %d", ret);
+		SEND_FATAL_ERROR();
+		return;
+	}
+
+	LOG_DBG("Submitting payload");
+
+	int err = zbus_chan_pub(&PAYLOAD_CHAN, &payload, K_NO_WAIT);
+	if (err) {
+		LOG_ERR("zbus_chan_pub, error: %d", err);
+		SEND_FATAL_ERROR();
+		return;
+	}
+}
+
+/* State handlers */
+
+static void state_init_run(void *obj)
+{
+	int err;
+	struct s_object const *state_object = obj;
+
+	if (&TIME_CHAN == state_object->chan) {
+		enum time_status time_status;
+
+		err = zbus_chan_read(&TIME_CHAN, &time_status, K_FOREVER);
+		if (err) {
+			LOG_ERR("zbus_chan_read, error: %d", err);
+			return;
+		}
+
+		if (time_status == TIME_AVAILABLE) {
+			LOG_DBG("Time available, sampling can start");
+
+			STATE_SET(STATE_SAMPLING);
+		}
+	}
+}
+
+
+static void state_sampling_run(void *obj)
+{
+	struct s_object const *state_object = obj;
+
+	if (&TRIGGER_CHAN == state_object->chan) {
+		int err;
+		enum trigger_type trigger_type;
+
+		err = zbus_chan_read(&TRIGGER_CHAN, &trigger_type, K_FOREVER);
+		if (err) {
+			LOG_ERR("zbus_chan_read, error: %d", err);
+			SEND_FATAL_ERROR();
+			return;
+		}
+
+		if (trigger_type == TRIGGER_DATA_SAMPLE) {
+			LOG_DBG("Data sample trigger received, getting energy estimate data");
+			sample_energy_estimate();
+		}
+	}
+}
+
+static void task_wdt_callback(int channel_id, void *user_data)
+{
+	LOG_ERR("Watchdog expired, Channel: %d, Thread: %s",
+		channel_id, k_thread_name_get((k_tid_t)user_data));
+
+	SEND_FATAL_ERROR_WATCHDOG_TIMEOUT();
+}
+
+
 static void network_task(void)
 {
 	int err;
+
+	STATE_SET_INITIAL(STATE_INIT);
 
 	/* Setup handler for Zephyr NET Connection Manager events. */
 	net_mgmt_init_event_callback(&l4_cb, l4_event_handler, L4_EVENT_MASK);
@@ -103,6 +255,41 @@ static void network_task(void)
 	 */
 	if (IS_ENABLED(CONFIG_BOARD_NATIVE_POSIX)) {
 		conn_mgr_mon_resend_status();
+	}
+
+	const struct zbus_channel *chan;
+	const uint32_t wdt_timeout_ms = (CONFIG_APP_NETWORK_WATCHDOG_TIMEOUT_SECONDS * MSEC_PER_SEC);
+	const uint32_t execution_time_ms = (CONFIG_APP_NETWORK_EXEC_TIME_SECONDS_MAX * MSEC_PER_SEC);
+	const k_timeout_t zbus_wait_ms = K_MSEC(wdt_timeout_ms - execution_time_ms);
+	int task_wdt_id;
+
+	task_wdt_id = task_wdt_add(wdt_timeout_ms, task_wdt_callback, (void *)k_current_get());
+
+	while (true) {
+		err = task_wdt_feed(task_wdt_id);
+		if (err) {
+			LOG_ERR("task_wdt_feed, error: %d", err);
+			SEND_FATAL_ERROR();
+			return;
+		}
+
+		err = zbus_sub_wait(&network, &chan, zbus_wait_ms);
+		if (err == -EAGAIN) {
+			continue;
+		} else if (err) {
+			LOG_ERR("zbus_sub_wait, error: %d", err);
+			SEND_FATAL_ERROR();
+			return;
+		}
+
+		s_obj.chan = chan;
+
+		err = STATE_RUN();
+		if (err) {
+			LOG_ERR("handle_message, error: %d", err);
+			SEND_FATAL_ERROR();
+			return;
+		}
 	}
 }
 
---
+++ tests/module/network/src/main.c
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#include <unity.h>
+
+#include <zephyr/fff.h>
+#include <zephyr/zbus/zbus.h>
+#include <zephyr/task_wdt/task_wdt.h>
+#include <zephyr/logging/log.h>
+#include "modem/lte_lc.h"
+#include "zephyr/net/net_mgmt.h"
+
+#include "message_channel.h"
+
+#include "zcbor_decode.h"
+#include "conn_info_object_decode.h"
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VALUE_FUNC(int, date_time_now, int64_t *);
+FAKE_VALUE_FUNC(int, task_wdt_feed, int);
+FAKE_VALUE_FUNC(int, task_wdt_add, uint32_t, task_wdt_callback_t, void *);
+FAKE_VALUE_FUNC(int, lte_lc_conn_eval_params_get, struct lte_lc_conn_eval_params *);
+FAKE_VALUE_FUNC(int, conn_mgr_all_if_connect, bool);
+FAKE_VALUE_FUNC(int, conn_mgr_all_if_up, bool);
+FAKE_VOID_FUNC(net_mgmt_add_event_callback, struct net_mgmt_event_callback *);
+
+LOG_MODULE_REGISTER(network_module_test, 4);
+
+ZBUS_MSG_SUBSCRIBER_DEFINE(test_subscriber);
+ZBUS_CHAN_ADD_OBS(PAYLOAD_CHAN, test_subscriber, 0);
+
+#define FAKE_TIME_MS 1716552398505
+#define FAKE_ENERGY_ESTIMATE 9
+
+static int date_time_now_custom_fake(int64_t *time)
+{
+	*time = FAKE_TIME_MS;
+	return 0;
+}
+
+static int lte_lc_conn_eval_params_get_custom_fake(struct lte_lc_conn_eval_params *params)
+{
+	params->energy_estimate = FAKE_ENERGY_ESTIMATE;
+
+	return 0;
+}
+
+static void send_time_available(void)
+{
+	enum time_status time_type = TIME_AVAILABLE;
+	int err = zbus_chan_pub(&TIME_CHAN, &time_type, K_SECONDS(1));
+
+	TEST_ASSERT_EQUAL(0, err);
+}
+
+void send_trigger(void)
+{
+	enum trigger_type trigger_type = TRIGGER_DATA_SAMPLE;
+	int err = zbus_chan_pub(&TRIGGER_CHAN, &trigger_type, K_SECONDS(1));
+
+	TEST_ASSERT_EQUAL(0, err);
+}
+
+void wait_for_and_decode_payload(struct energy_estimate *energy_estimate)
+{
+	const struct zbus_channel *chan;
+	static struct payload payload;
+	int err;
+
+	/* Allow the test thread to sleep so that the DUT's thread is allowed to run. */
+	k_sleep(K_MSEC(100));
+
+	err = zbus_sub_wait_msg(&test_subscriber, &chan, &payload, K_MSEC(1000));
+	if (err == -ENOMSG) {
+		LOG_ERR("No payload message received");
+		TEST_FAIL();
+	} else if (err) {
+		LOG_ERR("zbus_sub_wait, error: %d", err);
+		SEND_FATAL_ERROR();
+
+		return;
+	}
+
+	/* check if chan is payload channel */
+	if (chan != &PAYLOAD_CHAN) {
+		LOG_ERR("Received message from wrong channel");
+		TEST_FAIL();
+	}
+
+	/* decode payload */
+	cbor_decode_conn_info_object(payload.string, payload.string_len, energy_estimate, NULL);
+	if (err != ZCBOR_SUCCESS) {
+		LOG_ERR("Failed to decode payload");
+		TEST_FAIL();
+	}
+}
+
+void setUp(void)
+{
+	RESET_FAKE(task_wdt_feed);
+	RESET_FAKE(task_wdt_add);
+	RESET_FAKE(date_time_now);
+	RESET_FAKE(lte_lc_conn_eval_params_get);
+	RESET_FAKE(conn_mgr_all_if_connect);
+	RESET_FAKE(conn_mgr_all_if_up);
+
+	date_time_now_fake.custom_fake = date_time_now_custom_fake;
+	send_time_available();
+}
+
+void tearDown(void)
+{
+	const struct zbus_channel *chan;
+	static struct payload received_payload;
+	int err;
+
+	err = zbus_sub_wait_msg(&test_subscriber, &chan, &received_payload, K_MSEC(1000));
+	if (err == 0) {
+		LOG_ERR("Unhandled message in payload channel");
+		TEST_FAIL();
+	}
+}
+
+
+void test_energy_estimate(void)
+{
+	static struct energy_estimate energy_estimate = {0};
+
+	/* Given */
+	lte_lc_conn_eval_params_get_fake.custom_fake = lte_lc_conn_eval_params_get_custom_fake;
+
+	/* When */
+	send_trigger();
+
+	/* Then */
+	wait_for_and_decode_payload(&energy_estimate);
+	TEST_ASSERT_EQUAL(FAKE_ENERGY_ESTIMATE, energy_estimate.vi);
+	TEST_ASSERT_EQUAL(FAKE_TIME_MS / 1000, energy_estimate.bt);
+}
+
+void test_no_events_on_zbus_until_watchdog_timeout(void)
+{
+	/* Wait without feeding any events to zbus until watch dog timeout. */
+	k_sleep(K_SECONDS(CONFIG_APP_NETWORK_WATCHDOG_TIMEOUT_SECONDS));
+
+	/* Check if the watchdog was fed atleast once.*/
+	TEST_ASSERT_GREATER_OR_EQUAL(1, task_wdt_feed_fake.call_count);
+}
+
+/* This is required to be added to each test. That is because unity's
+ * main may return nonzero, while zephyr's main currently must
+ * return 0 in all cases (other values are reserved).
+ */
+extern int unity_main(void);
+
+int main(void)
+{
+	/* use the runner from test_runner_generate() */
+	(void)unity_main();
+
+	return 0;
+}
