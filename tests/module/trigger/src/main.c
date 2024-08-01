290336a7def1e8f911981aaec89d227a8cfc0817
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2024-08-01 15:42:52 +0200

    trigger: Fix frequent poll duration timer related bugs
    - Fix a bug where the frequent poll duration timer was not stopped on
    disconnect event. This lead to fault transitions when it expired.
    - Fix a bug where when the frequent poll duration timer expires in
    blocked state, there will be a sample trigger sent. This was because
    the state machine was transitioning to normal state and executing normal
    mode entry functions. This is now fixed by only changing the trigger
    mode in the state object but staying in the blocked state.
    
    Add native_sim based unit test for trigger module. Covers most of the
    state transitions. Fixed a few bugs discovered in the trigger module.
    
    Not tested yet:
    - Button press in blocked state
    - config rcvd in blocked state
    - Button press in frequent poll state
    - config rcvd in frequent poll state
    
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

---
+++ tests/module/trigger/src/main.c
@@ -0,0 +1,364 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#include <unity.h>
+
+#include <zephyr/fff.h>
+#include <zephyr/zbus/zbus.h>
+#include <zephyr/task_wdt/task_wdt.h>
+#include <zephyr/logging/log.h>
+#include "dk_buttons_and_leds.h"
+#include "message_channel.h"
+
+#define FREQUENT_POLL_TRIGGER_INTERVAL_SEC 20
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VALUE_FUNC(int, dk_buttons_init, button_handler_t);
+FAKE_VALUE_FUNC(int, task_wdt_feed, int);
+FAKE_VALUE_FUNC(int, task_wdt_add, uint32_t, task_wdt_callback_t, void *);
+
+LOG_MODULE_REGISTER(trigger_module_test, 4);
+
+ZBUS_MSG_SUBSCRIBER_DEFINE(trig_mode_subscriber);
+ZBUS_CHAN_ADD_OBS(TRIGGER_MODE_CHAN, trig_mode_subscriber, 0);
+
+ZBUS_MSG_SUBSCRIBER_DEFINE(trigger_subscriber);
+ZBUS_CHAN_ADD_OBS(TRIGGER_CHAN, trigger_subscriber, 0);
+
+static button_handler_t test_button_handler;
+
+static int dk_buttons_init_custom_fake(button_handler_t button_handler)
+{
+	test_button_handler = button_handler;
+
+	return 0;
+}
+
+void setUp(void)
+{
+	RESET_FAKE(task_wdt_feed);
+	RESET_FAKE(task_wdt_add);
+}
+
+static void send_cloud_connected_ready_to_send(void)
+{
+	enum cloud_status status = CLOUD_CONNECTED_READY_TO_SEND;
+	int err = zbus_chan_pub(&CLOUD_CHAN, &status, K_SECONDS(1));
+
+	TEST_ASSERT_EQUAL(0, err);
+}
+
+static void send_config(void)
+{
+#define TEST_UPDATE_INTERVAL_IN_SECONDS 3600
+
+	const struct configuration config = {
+		.update_interval_present = true,
+		.update_interval = TEST_UPDATE_INTERVAL_IN_SECONDS,
+	};
+	int err = zbus_chan_pub(&CONFIG_CHAN, &config, K_SECONDS(1));
+
+	TEST_ASSERT_EQUAL(0, err);
+}
+
+static void send_cloud_disconnected(void)
+{
+	enum cloud_status status = CLOUD_DISCONNECTED;
+	int err = zbus_chan_pub(&CLOUD_CHAN, &status, K_SECONDS(1));
+
+	TEST_ASSERT_EQUAL(0, err);
+}
+
+static void send_gnss_event(enum location_status location_status)
+{
+	int err = zbus_chan_pub(&LOCATION_CHAN, &location_status, K_SECONDS(1));
+
+	TEST_ASSERT_EQUAL(0, err);
+}
+
+static void check_trigger_mode_event(enum trigger_mode expected_trigger_mode)
+{
+	const struct zbus_channel *chan;
+	enum trigger_mode trigger_mode;
+	int err;
+
+	/* Allow the test thread to sleep so that the DUT's thread is allowed to run. */
+	k_sleep(K_MSEC(100));
+
+	err = zbus_sub_wait_msg(&trig_mode_subscriber, &chan, &trigger_mode, K_MSEC(1000));
+	if (err == -ENOMSG) {
+		LOG_ERR("No trigger mode received");
+		TEST_FAIL();
+	} else if (err) {
+		LOG_ERR("zbus_sub_wait, error: %d", err);
+		SEND_FATAL_ERROR();
+
+		return;
+	}
+
+	if (chan != &TRIGGER_MODE_CHAN) {
+		LOG_ERR("Received message from wrong channel");
+		TEST_FAIL();
+	}
+
+	TEST_ASSERT_EQUAL(expected_trigger_mode, trigger_mode);
+}
+
+static void check_trigger_event(enum trigger_type expected_trig_type)
+{
+	const struct zbus_channel *chan;
+	enum trigger_type trig_type;
+	int err;
+
+	/* Allow the test thread to sleep so that the DUT's thread is allowed to run. */
+	k_sleep(K_MSEC(100));
+
+	err = zbus_sub_wait_msg(&trigger_subscriber, &chan, &trig_type, K_MSEC(1000));
+	if (err == -ENOMSG) {
+		LOG_ERR("No trigger event received");
+		TEST_FAIL();
+	} else if (err) {
+		LOG_ERR("zbus_sub_wait, error: %d", err);
+		SEND_FATAL_ERROR();
+
+		return;
+	}
+
+	if (chan != &TRIGGER_CHAN) {
+		LOG_ERR("Received message from wrong channel");
+		TEST_FAIL();
+	}
+	TEST_ASSERT_EQUAL(expected_trig_type, trig_type);
+}
+
+static void check_no_trigger_events(uint32_t time_in_seconds)
+{
+	const struct zbus_channel *chan;
+	enum trigger_type trig_type;
+	int err;
+
+	/* Allow the test thread to sleep so that the DUT's thread is allowed to run. */
+	k_sleep(K_SECONDS(time_in_seconds));
+
+	err = zbus_sub_wait_msg(&trigger_subscriber, &chan, &trig_type, K_MSEC(1000));
+	if (err == 0) {
+		LOG_ERR("Received trigger event with type %d", trig_type);
+		TEST_FAIL();
+	}
+}
+
+static void check_no_trigger_mode_events(uint32_t time_in_seconds)
+{
+	const struct zbus_channel *chan;
+	enum trigger_mode trig_mode;
+	int err;
+
+	/* Allow the test thread to sleep so that the DUT's thread is allowed to run. */
+	k_sleep(K_SECONDS(time_in_seconds));
+
+	err = zbus_sub_wait_msg(&trig_mode_subscriber, &chan, &trig_mode, K_MSEC(1000));
+	if (err == 0) {
+		LOG_ERR("Received trigger mode event with type %d", trig_mode);
+		TEST_FAIL();
+	}
+}
+
+static void send_frequent_poll_duration_timer_expiry(void)
+{
+	/* Wait until CONFIG_FREQUENT_POLL_DURATION_INTERVAL_SEC */
+	k_sleep(K_SECONDS(CONFIG_FREQUENT_POLL_DURATION_INTERVAL_SEC));
+
+	/* Check if the required number of trigger events was sent during frequent poll state.
+	   We reduce 1 from the expected number of trigger events because one trigger event is sent
+	   when entering the frequent poll state. */
+	for (int i = 0;
+	     i <
+	     (CONFIG_FREQUENT_POLL_DURATION_INTERVAL_SEC / FREQUENT_POLL_TRIGGER_INTERVAL_SEC) - 1;
+	     i++) {
+		check_trigger_event(TRIGGER_DATA_SAMPLE);
+		check_trigger_event(TRIGGER_POLL);
+	}
+}
+
+static void go_to_frequent_poll_state(void)
+{
+	send_cloud_connected_ready_to_send();
+
+	check_trigger_mode_event(TRIGGER_MODE_POLL);
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+}
+
+static void go_to_normal_state(void)
+{
+	go_to_frequent_poll_state();
+	send_frequent_poll_duration_timer_expiry();
+	check_trigger_mode_event(TRIGGER_MODE_NORMAL);
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+}
+
+void test_init_to_frequent_poll(void)
+{
+	/* When */
+	send_cloud_connected_ready_to_send();
+
+	/* Then */
+	check_trigger_mode_event(TRIGGER_MODE_POLL);
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+void test_frequent_poll_to_normal(void)
+{
+	/* Given */
+	go_to_frequent_poll_state();
+
+	/* When */
+	send_frequent_poll_duration_timer_expiry();
+
+	/* Then */
+	check_trigger_mode_event(TRIGGER_MODE_NORMAL);
+	/* Verify that one more trigger event was sent when entering normal mode. */
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+	check_no_trigger_events(FREQUENT_POLL_TRIGGER_INTERVAL_SEC * 10);
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+void test_frequent_poll_to_blocked(void)
+{
+	/* Given */
+	go_to_frequent_poll_state();
+
+	/* When */
+	send_gnss_event(GNSS_ENABLED);
+
+	/* Then */
+	check_no_trigger_events(FREQUENT_POLL_TRIGGER_INTERVAL_SEC * 2);
+
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+void test_normal_to_blocked(void)
+{
+	/* Given */
+	go_to_normal_state();
+
+	/* When */
+	send_gnss_event(GNSS_ENABLED);
+
+	/* Then */
+	check_no_trigger_events(CONFIG_APP_TRIGGER_TIMEOUT_SECONDS * 2);
+
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+void test_normal_mode_to_frequent_poll_due_to_button_press(void)
+{
+	/* Given */
+	go_to_normal_state();
+
+	/* When */
+	test_button_handler(DK_BTN1_MSK, true);
+
+	/* Then */
+	check_trigger_mode_event(TRIGGER_MODE_POLL);
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+void test_normal_mode_to_frequent_poll_due_to_config_update(void)
+{
+	/* Given */
+	go_to_normal_state();
+
+	/* When */
+	send_config();
+
+	/* Then */
+	check_trigger_mode_event(TRIGGER_MODE_POLL);
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+void test_frequent_poll_to_blocked_to_frequent_poll(void)
+{
+	/* Given */
+	go_to_frequent_poll_state();
+	send_gnss_event(GNSS_ENABLED);
+
+	/* When */
+	send_gnss_event(GNSS_DISABLED);
+
+	/* Then */
+	k_sleep(K_SECONDS(FREQUENT_POLL_TRIGGER_INTERVAL_SEC));
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+	check_no_trigger_mode_events(1);
+
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+void test_frequent_poll_to_blocked_to_normal(void)
+{
+	/* Given */
+	go_to_frequent_poll_state();
+	send_gnss_event(GNSS_ENABLED);
+
+	/* When */
+	k_sleep(K_SECONDS(CONFIG_FREQUENT_POLL_DURATION_INTERVAL_SEC));
+	send_gnss_event(GNSS_DISABLED);
+
+	/* Then */
+	check_trigger_mode_event(TRIGGER_MODE_NORMAL);
+	check_trigger_event(TRIGGER_DATA_SAMPLE);
+	check_trigger_event(TRIGGER_POLL);
+	check_no_trigger_events(FREQUENT_POLL_TRIGGER_INTERVAL_SEC * 10);
+
+	/* Cleanup */
+	send_cloud_disconnected();
+}
+
+/* This is required to be added to each test. That is because unity's
+ * main may return nonzero, while zephyr's main currently must
+ * return 0 in all cases (other values are reserved).
+ */
+extern int unity_main(void);
+
+int main(void)
+{
+	(void)unity_main();
+
+	k_sleep(K_FOREVER);
+
+	return 0;
+}
+
+static int test_init(void)
+{
+	LOG_WRN("Running test_init");
+	dk_buttons_init_fake.custom_fake = dk_buttons_init_custom_fake;
+	return 0;
+}
+
+/* Initialize the dk_buttons_init_fake.custom_fake before DUT's init function is called by
+ * SYS_INIT()
+ */
+SYS_INIT(test_init, POST_KERNEL, CONFIG_APPLICATION_INIT_PRIORITY);
