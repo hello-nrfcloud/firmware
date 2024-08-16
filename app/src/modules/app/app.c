02da47acef3f6c43a2217336df269bee0a3acaa6
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2024-08-16 12:42:09 +0200

    test: test compliance
    testing compliance
    
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

--- app/src/modules/app/app.c
+++ app/src/modules/app/app.c
@@ -260,6 +260,7 @@ K_THREAD_DEFINE(app_task_id,
 static int watchdog_init(void)
 {
 	__ASSERT((task_wdt_init(NULL) == 0), "Task watchdog init failure");
+	/* This is an intentionally longgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg line.*/
 
 	return 0;
 }
