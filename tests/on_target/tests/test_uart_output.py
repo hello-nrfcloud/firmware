1d484fe8d87e007b1e2b370a72691f3c4a87c5a6
Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>
2024-09-19 13:07:16 +0200

    test commit

--- tests/on_target/tests/test_uart_output.py
+++ tests/on_target/tests/test_uart_output.py
@@ -14,50 +14,56 @@ from utils.logger import get_logger
 
 logger = get_logger()
 
+'''
+TEST DOCSTRING: test_uart_output module
+'''
+
 @pytest.mark.dut1
 @pytest.mark.uart
 def test_uart_output(t91x_board, hex_file):
-    flash_device(os.path.abspath(hex_file))
-    time.sleep(5)
-    t91x_board.uart.xfactoryreset()
-    patterns_boot = [
-            "Network connectivity established",
-            "Connected to Cloud",
-            "trigger: trigger_work_fn: Sending data sample trigger",
-            "battery: sample: State of charge:",
-            "environmental_module: sample: temp:",
-            "transport: state_connected_ready_run: Payload",
-            "blocked_run: Location search done",
-            "trigger: frequent_poll_entry: frequent_poll_entry"
-    ]
-    patterns_button_press = [
-        "trigger: frequent_poll_run: Button 1 pressed in frequent poll state, restarting duration timer",
-        "trigger_poll_work_fn: Sending shadow/fota poll trigger"
-    ]
-    patterns_lte_offline = [
-            "network: Network connectivity lost",
-    ]
-    patterns_lte_normal = [
-            "network: Network connectivity established",
-            "transport: Connected to Cloud"
-    ]
+    '''TEST DOCSTRING: test_uart_output function'''
+    logger.info("test uart")
+    # flash_device(os.path.abspath(hex_file))
+    # time.sleep(5)
+    # t91x_board.uart.xfactoryreset()
+    # patterns_boot = [
+    #         "Network connectivity established",
+    #         "Connected to Cloud",
+    #         "trigger: trigger_work_fn: Sending data sample trigger",
+    #         "battery: sample: State of charge:",
+    #         "environmental_module: sample: temp:",
+    #         "transport: state_connected_ready_run: Payload",
+    #         "blocked_run: Location search done",
+    #         "trigger: frequent_poll_entry: frequent_poll_entry"
+    # ]
+    # patterns_button_press = [
+    #     "trigger: frequent_poll_run: Button 1 pressed in frequent poll state, restarting duration timer",
+    #     "trigger_poll_work_fn: Sending shadow/fota poll trigger"
+    # ]
+    # patterns_lte_offline = [
+    #         "network: Network connectivity lost",
+    # ]
+    # patterns_lte_normal = [
+    #         "network: Network connectivity established",
+    #         "transport: Connected to Cloud"
+    # ]
 
-    # Boot
-    t91x_board.uart.flush()
-    reset_device()
-    t91x_board.uart.wait_for_str(patterns_boot, timeout=120)
+    # # Boot
+    # t91x_board.uart.flush()
+    # reset_device()
+    # t91x_board.uart.wait_for_str(patterns_boot, timeout=120)
 
-    # Button press
-    t91x_board.uart.flush()
-    t91x_board.uart.write("zbus button_press\r\n")
-    t91x_board.uart.wait_for_str(patterns_button_press, timeout=120)
+    # # Button press
+    # t91x_board.uart.flush()
+    # t91x_board.uart.write("zbus button_press\r\n")
+    # t91x_board.uart.wait_for_str(patterns_button_press, timeout=120)
 
-    # LTE disconnect
-    t91x_board.uart.flush()
-    t91x_board.uart.write("lte offline\r\n")
-    t91x_board.uart.wait_for_str(patterns_lte_offline, timeout=20)
+    # # LTE disconnect
+    # t91x_board.uart.flush()
+    # t91x_board.uart.write("lte offline\r\n")
+    # t91x_board.uart.wait_for_str(patterns_lte_offline, timeout=20)
 
-    # LTE reconnect
-    t91x_board.uart.flush()
-    t91x_board.uart.write("lte normal\r\n")
-    t91x_board.uart.wait_for_str(patterns_lte_normal, timeout=120)
+    # # LTE reconnect
+    # t91x_board.uart.flush()
+    # t91x_board.uart.write("lte normal\r\n")
+    # t91x_board.uart.wait_for_str(patterns_lte_normal, timeout=120)
