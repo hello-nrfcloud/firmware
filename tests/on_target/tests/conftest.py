3fcf7859fa0ffaa8f331578108b9ac0c341a7bb1
Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>
2024-11-11 15:23:35 +0100

    tests: on_target: add ppk test
    Add ppk test and update nightly badge accordingly.
    Also, add html interactive power plot, hosted on github pages.
    
    Signed-off-by: Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>

--- tests/on_target/tests/conftest.py
+++ tests/on_target/tests/conftest.py
@@ -102,7 +102,7 @@ def t91x_traces(t91x_board):
 def hex_file():
     # Search for the firmware hex file in the artifacts folder
     artifacts_dir = "artifacts"
-    hex_pattern = r"hello\.nrfcloud\.com-[a-f0-9]+-thingy91x-nrf91\.hex"
+    hex_pattern = r"hello\.nrfcloud\.com-[0-9a-z\.]+-thingy91x-nrf91\.hex"
 
     for file in os.listdir(artifacts_dir):
         if re.match(hex_pattern, file):
