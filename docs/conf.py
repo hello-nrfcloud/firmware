18717386ea8b29ca7cc6db0a59c05f4d9cb9b4c3
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2024-11-28 13:33:29 +0100

    docs: Update configuration and python requirements
    Update configuration and requirements to be able to build
    the documentation.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- docs/conf.py
+++ docs/conf.py
@@ -6,15 +6,29 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'hello-nrfcloud firmware'
+from pathlib import Path
+import sys
+
+project = 'Thingy:91 X Out-of-Box firmware'
 copyright = '2024, Nordic Semiconductor'
 author = 'Nordic Semiconductor'
 release = '1.0.0'
 
+# Paths
+
+DOC_BASE = Path(__file__).absolute()
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+sys.path.insert(0, str("_extensions"))
+
+extensions = [
+    'breathe',
+    'sphinxcontrib.mscgen',
+    'sphinx_tabs.tabs',
+    'sphinx_togglebutton',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build_sphinx', 'Thumbs.db', '.DS_Store']
