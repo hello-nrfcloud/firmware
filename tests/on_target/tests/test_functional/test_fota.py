23b75490a23b2470381990172c3a586ac5988848
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2025-11-12 13:22:55 +0100

    tests: Update to mfw 2.0.3
    Signed-off-by: Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>

--- tests/on_target/tests/test_functional/test_fota.py
+++ tests/on_target/tests/test_functional/test_fota.py
@@ -12,9 +12,9 @@ from utils.logger import get_logger
 
 logger = get_logger()
 
-MFW_202_FILEPATH = "artifacts/mfw_nrf91x1_2.0.2.zip"
-DELTA_MFW_BUNDLEID = "MODEM*3471f88e*mfw_nrf91x1_2.0.2-FOTA-TEST"
-FULL_MFW_BUNDLEID = "MDM_FULL*124c2b20*mfw_nrf91x1_full_2.0.2"
+MFW_FILEPATH = "artifacts/mfw_nrf91x1_2.0.3.zip"
+DELTA_MFW_BUNDLEID = "c8443b86-d295-4305-8e39-0ae2731178e1"
+FULL_MFW_BUNDLEID = "61cedb8d-0b6f-4684-9150-5aa782c6c8d5"
 APP_BUNDLEID = "APP*a82b7076*v2.0.1"
 
 APP_FOTA_TIMEOUT = 60 * 10
@@ -27,25 +27,6 @@ def post_job(t91x_fota, bundle_id, fota_type):
     t91x_fota.uart.flush()
     return
 
-def run_fota_resumption(t91x_fota, fota_type):
-    t91x_fota.uart.wait_for_str("50%")
-    logger.debug(f"Testing fota resumption on disconnect for {fota_type} fota")
-
-    patterns_lte_offline = ["network: Network connectivity lost"]
-    patterns_lte_normal = ["network: Network connectivity established"]
-
-    # LTE disconnect
-    t91x_fota.uart.flush()
-    t91x_fota.uart.write("lte offline\r\n")
-    t91x_fota.uart.wait_for_str(patterns_lte_offline, timeout=20)
-
-    # LTE reconnect
-    t91x_fota.uart.flush()
-    t91x_fota.uart.write("lte normal\r\n")
-    t91x_fota.uart.wait_for_str(patterns_lte_normal, timeout=120)
-
-    t91x_fota.uart.wait_for_str("fota_download: Refuse fragment, restart with offset")
-    t91x_fota.uart.wait_for_str("fota_download: Downloading from offset:")
 
 @pytest.fixture
 def run_fota_fixture(t91x_fota, hex_file):
@@ -57,9 +38,6 @@ def run_fota_fixture(t91x_fota, hex_file):
         t91x_fota.uart.wait_for_str("Connected to Cloud")
 
         post_job(t91x_fota, bundleId, fota_type)
-
-        # if test_fota_resumption:
-        #     run_fota_resumption(t91x_fota, fota_type)
 
         t91x_fota.uart.flush()
         t91x_fota.uart.wait_for_str("FOTA download finished", timeout=fotatimeout)
@@ -85,15 +63,12 @@ def test_delta_mfw_fota(run_fota_fixture):
     '''
     Test delta modem FOTA on nrf9151
     '''
-    # Flash with mfw202
-    flash_device(os.path.abspath(MFW_202_FILEPATH))
+    flash_device(os.path.abspath(MFW_FILEPATH))
 
     try:
-        # run_fota(DELTA_MFW_BUNDLEID, hex_file)
         run_fota_fixture(DELTA_MFW_BUNDLEID, "delta")
     finally:
-        # Restore mfw202, no matter if test pass/fails
-        flash_device(os.path.abspath(MFW_202_FILEPATH))
+        flash_device(os.path.abspath(MFW_FILEPATH))
 
 @pytest.mark.slow
 def test_full_mfw_fota(run_fota_fixture):
