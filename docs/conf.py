4483d46387b0ca3db5b36d5a3847363eade81d08
divya pillai <divya.pillai@nordicsemi.no>
2026-05-21 14:18:01 +0200

    doc: Temp revert of new theme
    Temporarily revert new theme
    as doc are not rendered in the Techdocs.
    
    Signed-off-by: divya pillai <divya.pillai@nordicsemi.no>

--- docs/conf.py
+++ docs/conf.py
@@ -26,11 +26,6 @@ exclude_patterns = ['_build_sphinx', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_ncs_theme'
-html_show_sphinx = False
-html_static_path = ["_static"]
-html_css_files = ["hide-versions.css"]
-
-html_theme_options = {'docsets': {},"addons_url": "https://nrfconnect.github.io/ncs-app-index/","bare_metal_url": "","ncs_url": "https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/index.html", "ncs_label": "nRF Connect SDK Docs", "logo_url": "https://docs.nordicsemi.com"}
 
 # Include following files at the end of each .rst file.
 rst_epilog = """
