16ed695c71c6d6c36c336ad29db5703ddbc33737
Divya S Pillai <91891495+divipillai@users.noreply.github.com>
2026-05-21 10:52:48 +0200

    doc: Add new theme to doc (#582)
    Add new theme to doc
    
    Signed-off-by: divya pillai <divya.pillai@nordicsemi.no>

--- docs/conf.py
+++ docs/conf.py
@@ -7,14 +7,17 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'hello-nrfcloud firmware'
-copyright = '2024, Nordic Semiconductor'
+copyright = '2026, Nordic Semiconductor'
 author = 'Nordic Semiconductor'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinxcontrib.jquery",
+    "sphinx_copybutton",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build_sphinx', 'Thumbs.db', '.DS_Store']
@@ -23,6 +26,11 @@ exclude_patterns = ['_build_sphinx', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_ncs_theme'
+html_show_sphinx = False
+html_static_path = ["_static"]
+html_css_files = ["hide-versions.css"]
+
+html_theme_options = {'docsets': {},"addons_url": "https://nrfconnect.github.io/ncs-app-index/","bare_metal_url": "","ncs_url": "https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/index.html", "ncs_label": "nRF Connect SDK Docs", "logo_url": "https://docs.nordicsemi.com"}
 
 # Include following files at the end of each .rst file.
 rst_epilog = """
